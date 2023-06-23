539bde483a65b9cde780f6f8bb21ba0448ba882d
Andrzej Leszkiewicz <59934292+avatorl@users.noreply.github.com>
2023-06-23 18:01:30 +0200

    Update ErrorCheckForDuplicates.m

--- ErrorCheckForDuplicates.m
+++ ErrorCheckForDuplicates.m
@@ -1,12 +1,12 @@
-//Chech if there are duplicate rows in the table. Return Error if any
-    _CountRows1 = Table.RowCount(<#"Previous Step">),
-    #"Removed Duplicates" = Table.Distinct(#"Previous Step", {<"Column Name">}),
+    //Chech if there are duplicate rows in the table. Return Error if any
+    _InputTable = #"Removed Other Columns",
+    _CountRows1 = Table.RowCount(_InputTable),
+    #"Removed Duplicates" = Table.Distinct(#"Previous Step", {"COLUMN_NAME"}),
     _CountRows2 = Table.RowCount(#"Removed Duplicates"),
     _Error = 
     error [ 
         Reason = "DuplicateRecords", 
         Message = "Duplicate records", 
-        Detail = "Duplicate records found in <[Column Name]> column" 
+        Detail = "Duplicate records found in the data" 
     ],
-    Result = if _CountRows2 = _CountRows1 then #"Removed Duplicates" else _Error
-    
+    _Output = if _CountRows2 = _CountRows1 then _InputTable else _Error,
