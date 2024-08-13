62b9ba15e652294070cb9cbddbb9d6ef1830ef1d
Andrzej Leszkiewicz <59934292+avatorl@users.noreply.github.com>
2024-08-14 01:50:50 +0200

    Update Date Table.m

--- Date Table.m
+++ Date Table.m
@@ -12,11 +12,12 @@ let
     //End date (manual entry)
     //_Date_End = #date(2024, 12, 31),
 
+    //First day of week
     _FirstDayOfWeek = Day.Monday,
     //First month of fiscal year (number)
     _FirstMonthOfFiscalYear = 7,
 
-    //Fact table date column (or any other list) that defines lits of required dates
+    //Fact table date column (or any other list that defines lits of required dates)
     Source = Financials[Month],
     ListDistinct = List.Buffer(List.Distinct(Source)),
 
@@ -34,16 +35,24 @@ let
     Changed_Type_Date = Table.TransformColumnTypes(Table_From_List,{{"Date", type date}}),
 
     Add_DateNumber = Table.AddColumn(Changed_Type_Date, "Date Number", each Number.From([Date]), Int64.Type),
+
+    //YEAR
+    
     Add_YYYYMMDD_Number = Table.AddColumn(Add_DateNumber, "YYYYMMDD Number", each Number.From(Date.ToText([Date], "yyyyMMdd")), Int64.Type),
     Add_YearNumber = Table.AddColumn(Add_YYYYMMDD_Number, "Year Number", each Date.Year([Date]), Int64.Type),
     Add_YYYY_MM = Table.AddColumn(Add_YearNumber, "YYYY-MM", each Date.ToText( [Date], "yyyy-MM"), type text),
 
+    //MONTH
+    
     AddMonthNumber = Table.AddColumn(Add_YYYY_MM, "Month Number", each Date.Month([Date]), Int64.Type),
     AddMonthName = Table.AddColumn(AddMonthNumber, "Month Long", each Date.MonthName([Date], "EN-us"), type text),
     AddMonthNameShort = Table.AddColumn(AddMonthName, "Month Short", each Date.ToText([Date], "MMM", "EN-us"), type text),
     AddMonthStart = Table.AddColumn(AddMonthNameShort, "Start of Month", each Date.StartOfMonth([Date]), type date),
     AddMonthEnd = Table.AddColumn(AddMonthStart, "End of Month", each Date.EndOfMonth([Date]), type date),
     AddDaysInMonth = Table.AddColumn(AddMonthEnd, "Days in Month", each Date.DaysInMonth([Date]), Int64.Type),
+
+    //ISO WEEK
+    
     AddISOWeek = Table.AddColumn(AddDaysInMonth, "ISO Week", each let
         CurrentThursday = Date.AddDays([Date], 3-Date.DayOfWeek([Date], Day.Monday)),
         YearCurrentThursday = Date.Year(CurrentThursday),
@@ -52,6 +61,8 @@ let
     in AddISOWeek, Int64.Type),
     AddISOYear = Table.AddColumn(AddISOWeek, "ISO Year", each Date.Year(Date.AddDays([Date], 26-[ISO Week])), Int64.Type),
 
+    //WEEK
+    
     AddWeekStart = Table.AddColumn(AddISOYear, "Start of Week", each Date.StartOfWeek([Date], _FirstDayOfWeek), type date),
     AddWeekEnd = Table.AddColumn(AddWeekStart, "End of Week", each Date.EndOfWeek([Date], _FirstDayOfWeek), type date),
     AddDayOfWeekNumber = Table.AddColumn(AddWeekEnd, "Day of Week Number", each Date.DayOfWeek([Date], _FirstDayOfWeek), Int64.Type),
@@ -61,8 +72,13 @@ let
     AddDayOfWeekShort3 = Table.AddColumn(AddDayOfWeekLong, "Day of Week Short 3", each Date.ToText([Date], "ddd", "EN-us"), type text),
     AddDayOfWeekShort2 = Table.AddColumn(AddDayOfWeekShort3, "Day of Week Short 2", each Text.Start([Day of Week Short 3], 2), type text),
 
+    //DAY
+    
     AddDayOfYear = Table.AddColumn(AddDayOfWeekShort2, "Day of Year", each Date.DayOfYear([Date]), Int64.Type),
     AddDayOfMonth = Table.AddColumn(AddDayOfYear, "Day of Month", each Date.Day([Date]), Int64.Type),
+
+    //QUARTER
+    
     AddQuarterNumber = Table.AddColumn(AddDayOfMonth, "Quarter Number", each Date.QuarterOfYear([Date]), Int64.Type),
     AddQuarter = Table.AddColumn(AddQuarterNumber, "Quarter", each "Q" & Text.From(Date.QuarterOfYear([Date])), type text),
     AddYearQuarter = Table.AddColumn(AddQuarter, "Year-Quarter", each Text.From(Date.Year([Date])) & "-Q" & Text.From( Date.QuarterOfYear([Date])), type text),
