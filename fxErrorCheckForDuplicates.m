306703f4d33bd2fd6f5829f2aedc58cef8316f34
Andrzej Leszkiewicz <59934292+avatorl@users.noreply.github.com>
2024-08-14 13:18:19 +0200

    Update and rename ErrorCheckForDuplicates.m to fxErrorCheckForDuplicates.m

---
+++ fxErrorCheckForDuplicates.m
@@ -0,0 +1,28 @@
+// Check if the specified column in the InputTable contains duplicate values.
+// Returns the InputTable if no duplicates are found; otherwise, raises an error.
+(
+    InputTable as table,  // Parameter for the input table
+    ColumnName as text    // Parameter for the column name to check for duplicates
+) =>
+let
+    // Count the number of rows in the original table
+    _CountRows1 = Table.RowCount(InputTable),
+    
+    // Remove duplicate rows based on the specified column
+    #"Removed Duplicates" = Table.Distinct(InputTable, {ColumnName}),
+    
+    // Count the number of rows after removing duplicates
+    _CountRows2 = Table.RowCount(#"Removed Duplicates"),
+    
+    // Define the error to be raised if duplicates are found
+    _Error = 
+    error [ 
+        Reason = "DuplicateRecords", 
+        Message = "Duplicate records", 
+        Detail = "Duplicate records found in the data" 
+    ],
+    
+    // Output the original table if no duplicates are found; otherwise, raise the error
+    _Output = if _CountRows2 = _CountRows1 then InputTable else _Error
+in
+    _Output
