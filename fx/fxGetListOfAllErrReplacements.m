f292b7abecb2cf1bcc7ad78c75a9c12984eda508
Andrzej Leszkiewicz <59934292+avatorl@users.noreply.github.com>
2024-08-14 13:32:27 +0200

    Update fxGetListOfAllErrReplacements.m

--- fx/fxGetListOfAllErrReplacements.m
+++ fx/fxGetListOfAllErrReplacements.m
@@ -1,28 +1,29 @@
 // Build a list of all table columns for Table.ReplaceErrorValues()
 // Output: {{"Column1", null}, {"Column2", null}}
+// Usage: Table.ReplaceErrorValues(table as table, errorReplacement as list) as table
 
 let 
     // Function to generate the list of column names paired with null values for error replacement
-    GetListOfAllErrReplacements = (_table as table) =>
+    ErrorReplacement = (_table as table) =>
     let
         // Get the list of all column names in the input table
         _AllColumns = Table.ColumnNames(_table),
 
         // Convert the list of column names into a single-column table
-        #"Converted to Table" = Table.FromList(_AllColumns, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+        #"Converted to Table" = Table.FromList(_AllColumns, Splitter.SplitByNothing(), {"ColumnName"}),
 
         // Add a new column with null values, corresponding to each column name
-        #"Added Custom1" = Table.AddColumn(#"Converted to Table", "Custom1", each null),
+        #"Added Null Column" = Table.AddColumn(#"Converted to Table", "NullValue", each null),
 
         // Combine each column name and its corresponding null value into a list (e.g., {"Column1", null})
-        #"Added Custom2" = Table.AddColumn(#"Added Custom1", "Custom2", each {[Column1], [Custom1]}),
+        #"Combined Columns" = Table.AddColumn(#"Added Null Column", "Replacement", each {[ColumnName], [NullValue]}),
 
         // Remove the intermediate columns, leaving only the combined column-name-null-value pairs
-        #"Removed Columns" = Table.RemoveColumns(#"Added Custom2", {"Column1", "Custom1"}),
+        #"Removed Intermediate Columns" = Table.RemoveColumns(#"Combined Columns", {"ColumnName", "NullValue"}),
 
         // Extract the list of all column-name-null-value pairs
-        _ListOfAllReplacements = #"Removed Columns"[Custom2]
+        _ListOfAllReplacements = #"Removed Intermediate Columns"[Replacement]
     in 
         _ListOfAllReplacements
 in
-    GetListOfAllErrReplacements
+    ErrorReplacement
