d9dd867843c74957250416c956c1eab5411bbf53
Andrzej Leszkiewicz <59934292+avatorl@users.noreply.github.com>
2025-07-24 22:53:53 +0200

    Update fxHttpRequestTest.m

--- fx/fxHttpRequestTest.m
+++ fx/fxHttpRequestTest.m
@@ -1,21 +1,21 @@
 (endpoint as text, optional from as text, optional to as text) =>
     let
-        _test = "testkeyword",
+        _keyword = "querytest",
         _from = if from = null then "" else Text.From(from),
         _to = if to = null then "" else Text.From(to),
 
                 // Headers
                 Headers = [
                     #"Content-Type" = "application/json",
-                    #"x-api-key" = "API_KEY"
+                    #"x-api-key" = "API_KEY_TEST"
                 ],
                 // HTTP request
                 Response = Web.Contents(
-                   "https://testxxx.free.beeceptor.com",
+                   "***.free.beeceptor.com",
                     [
                         Headers = Headers,
                         RelativePath = endpoint,
-                        Query = [test = _test, from = _from, to = _to]
+                        Query = [test = _keyword, from = _from, to = _to]
                     ]
                 )               
     in
